051999db57deb7d2399117a19373feebd641e77d
David Richards <davidlamontrichards@gmail.com>
2012-04-03 14:30:53 -0600

    Living with a half-finished to_json until after I get Mutability and Associations mixed in.

--- lib/gearbox/mixins/active_model_implementation.rb
+++ lib/gearbox/mixins/active_model_implementation.rb
@@ -7,6 +7,14 @@ module Gearbox
     def self.included(base)
       base.send :include, ActiveModel::Validations
       base.send :include, ActiveModel::Conversion
+
+      # This isn't right...so I need to research these things a little bit. 
+      # What I'm thinking is a RESTful API isn't too much to ask from Gearbox, 
+      # So I want these to produce JSON and XML in a consistent way...
+      # I may be wrong, but I may be getting the RDF::JSON version here instead.
+      base.send :include, ActiveModel::Serializers::JSON
+      base.send :include, ActiveModel::Serializers::Xml
+      
       base.send :extend, ActiveModel::Naming
     end
     
