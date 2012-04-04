e2a2c786931d53111e0aa16903bd4ff5336e90b6
David Richards <davidlamontrichards@gmail.com>
2012-04-04 05:35:48 -0600

    Pry utilities update

--- lib/pry_utilities.rb
+++ lib/pry_utilities.rb
@@ -58,6 +58,7 @@ class Utilities
   
   def list_models
     Dir.glob("#{model_directory}/*.rb").map { |file| File.basename(file).split('.')[0..-2].join('.').to_sym}
+    true
   end
   
   def tmp_directory
